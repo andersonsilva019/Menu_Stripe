f06915ed8cdfac4382fdf0d3efeda6de6fb8beb6
Anderson0017 <andeerson0017@gmail.com>
2020-07-23 08:05:17 -0300

    create layout and struct

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>teste</h1>
+    <>
+      <Layout>
+        <h1>teste</h1>
+      </Layout>
+      <GlobalStyles/>
+    </>
   );
 }
 
